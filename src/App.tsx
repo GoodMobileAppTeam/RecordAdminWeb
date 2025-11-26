3e4c2c8c8a403d2a9e1f8166c9c12b463a5c29e6
bikooju <jhjk1234@gmail.com>
2025-11-27 00:24:16 +0900

    :gear: initial setting

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Admin Frontend + Tailwind 설정 완료!
+      </h1>
+    </div>
+  );
+}
+export default App;
