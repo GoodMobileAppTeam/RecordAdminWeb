a584625f7c92b7f3fc6c33c0c9b3f29b469ecf9a
bikooju <jhjk1234@gmail.com>
2025-11-29 01:19:24 +0900

    :recycle: 라이브러리 추가 및 파일 절대경로로 관리

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
 
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "src") },
+      { find: "@api", replacement: path.resolve(__dirname, "src/api") },
+      {
+        find: "@components",
+        replacement: path.resolve(__dirname, "src/components"),
+      },
+      { find: "@hooks", replacement: path.resolve(__dirname, "src/hooks") },
+      { find: "@layouts", replacement: path.resolve(__dirname, "src/layouts") },
+      { find: "@pages", replacement: path.resolve(__dirname, "src/pages") },
+      { find: "@routes", replacement: path.resolve(__dirname, "src/routes") },
+      { find: "@types", replacement: path.resolve(__dirname, "src/types") },
+    ],
+  },
+});
